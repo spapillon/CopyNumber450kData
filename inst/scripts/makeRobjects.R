b31ae5b3855f274d62c8b3fe4dc0e6ec9707c7a4
Simon Papillon <papillonsimon@gmail.com>
2013-12-20 18:12:05 -0500

    Removed stuff

--- inst/scripts/makeRobjects.R
+++
@@ -1,4 +0,0 @@
-library(minfi)
-sheet <- read.450k.sheet("../extdata", pattern = "csv$")
-RGcontrolSetEx <- read.450k.exp(targets = sheet)
-save(RGcontrolSetEx, file = "../../data/RGcontrolSetEx.rda", compress = "xz")
